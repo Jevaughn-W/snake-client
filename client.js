e0e9415fdf468d7e6cf71ea0d4bb69d2219645b1
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-09-07 19:32:43 -0400

    add callbck to write username to server

--- client.js
+++ client.js
@@ -1,3 +1,4 @@
+const { Server } = require("http");
 const net = require("net");
 
 // establishes a connection with the game server
@@ -7,9 +8,18 @@ const connect = function () {
     port: 50541,
   });
 
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server'); // Writes upon connecting with the server
+  });
+  
+  conn.on('connect', () => { // Send user name upon connecting to client
+    conn.write("Name: JJW");
+  });
+
   conn.on('data', (data) => {
     console.log(data);
   });
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
