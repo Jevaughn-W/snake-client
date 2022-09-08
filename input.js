e860dd9901c76c9a261359cc1195812d0c7efdbc
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-09-07 22:12:13 -0400

    remove setInterval to eliminate bug

--- input.js
+++ input.js
@@ -17,17 +17,17 @@ const handleUserInput = function () {  // Assigns actions to the user input
       case '\u0003':
         console.log("Exiting ...");
         process.exit();
-      case "w": 
-        setInterval( () => connection.write("Move: up"), 500);
+      case "w":   
+        connection.write("Move: up");
         break;
       case "a": 
-        setInterval( () => connection.write("Move: left"), 500);
+        connection.write("Move: left");
         break;
       case "s": 
-        setInterval( () => connection.write("Move: down"), 500);
+        connection.write("Move: down");
         break;
       case "d": 
-        setInterval( () => connection.write("Move: right"), 500);
+        connection.write("Move: right");
         break;
       default: break; // seems to have a run each key in successive increments after the previous
     }
