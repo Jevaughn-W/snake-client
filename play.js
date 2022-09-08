ca249397123747219a9b9eb6f9d1f63eff397dd2
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-09-07 20:39:52 -0400

    Add ability to read user input and exit program

--- play.js
+++ play.js
@@ -1,4 +1,27 @@
+const { stdin } = require('process');
 const {connect} = require ('./client');
 
 console.log("Connecting ...");
 connect();
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+
+const handleUserInput = function () {
+  process.stdin.on('data', (key) =>  {
+    if (key === '\u0003') {
+      console.log("Exiting ...")
+      process.exit();
+    }
+  });
+};
+
+stdin.on("data", handleUserInput);
+
+setupInput();
