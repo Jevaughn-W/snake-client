46cf4b0b1042528f8e1f65a5697bd4344311ca28
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-09-07 21:52:51 -0400

    Connect key input to write to server

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
 const { stdin } = require('process');
+let connection;
 
-const setupInput = function () { // Allows the user input to be read
+const setupInput = (conn) => { // Allows the user input to be read
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -10,9 +12,24 @@ const setupInput = function () { // Allows the user input to be read
 
 const handleUserInput = function () {  // Assigns actions to the user input
   process.stdin.on('data', (key) =>  {
-    if (key === '\u0003') {
-      console.log("Exiting ...")
-      process.exit();
+    
+    switch (key) {  //creates the appropriate movement based on the user input
+      case '\u0003':
+        console.log("Exiting ...");
+        process.exit();
+      case "w": 
+        setInterval( () => connection.write("Move: up"), 500);
+        break;
+      case "a": 
+        setInterval( () => connection.write("Move: left"), 500);
+        break;
+      case "s": 
+        setInterval( () => connection.write("Move: down"), 500);
+        break;
+      case "d": 
+        setInterval( () => connection.write("Move: right"), 500);
+        break;
+      default: break; // seems to have a run each key in successive increments after the previous
     }
   });
 };
