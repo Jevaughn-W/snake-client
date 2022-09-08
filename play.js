83ff6768d4ccad16441be129d523177b788fd62b
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2022-09-07 20:57:24 -0400

    refactor code to remove input function definition

--- play.js
+++ play.js
@@ -1,27 +1,6 @@
-const { stdin } = require('process');
 const {connect} = require ('./client');
+const {setupInput} = require ('./input');
 
 console.log("Connecting ...");
 connect();
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-};
-
-
-const handleUserInput = function () {
-  process.stdin.on('data', (key) =>  {
-    if (key === '\u0003') {
-      console.log("Exiting ...")
-      process.exit();
-    }
-  });
-};
-
-stdin.on("data", handleUserInput);
-
 setupInput();
